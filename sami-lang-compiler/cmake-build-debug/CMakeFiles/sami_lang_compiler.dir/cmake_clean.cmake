921f42da19371c407132056b591b7add0074f769
WarriorsSami <sbarbutdica@gmail.com>
2021-08-31 13:00:22 +0300

    ops(folder structure): add .sm example and compiler folder

---
+++ sami-lang-compiler/cmake-build-debug/CMakeFiles/sami_lang_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sami_lang_compiler.dir/main.c.o"
+  "sami_lang_compiler"
+  "sami_lang_compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sami_lang_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
