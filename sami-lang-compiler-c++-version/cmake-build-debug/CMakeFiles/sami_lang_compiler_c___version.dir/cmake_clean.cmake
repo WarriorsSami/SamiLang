8e2610e1bf11c8bab8cbaf14fa5c41d434adbc61
WarriorsSami <sbarbutdica@gmail.com>
2021-09-07 20:23:28 +0300

    feat(switch): use c++ alongside with rust to enhance oop styling for the entire compiler

---
+++ sami-lang-compiler-c++-version/cmake-build-debug/CMakeFiles/sami_lang_compiler_c___version.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sami_lang_compiler_c___version.dir/main.cpp.o"
+  "sami_lang_compiler_c___version"
+  "sami_lang_compiler_c___version.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sami_lang_compiler_c___version.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sami-lang-compiler-c++-version/cmake-build-debug/CMakeFiles/sami_lang_compiler_c___version.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sami_lang_compiler_c___version.
+# This may be replaced when dependencies are built.
