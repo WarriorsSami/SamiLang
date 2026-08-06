b012e2cbdc6d9e1b23b8d128cdcdd0fb711fc47d
WarriorsSami <sbarbutdica@gmail.com>
2026-08-06 19:56:56 +0300

    feat(rust+lexer): add support for the rest of keywords and operators

--- sami-lang-compiler-rust-version/src/error.rs
+++ sami-lang-compiler-rust-version/src/error.rs
@@ -1,7 +1,11 @@
 use thiserror::Error;
 
-#[derive(Error, Debug)]
-pub enum CompileError {
-    #[error("invalid token `{0}` encountered during lexical analysis")]
-    InvalidToken(String),
+#[derive(Error, Debug, Eq, PartialEq)]
+pub enum LexError {
+    #[error("invalid token `{0}` encountered")]
+    InvalidToken(char),
+    #[error(r#"no matching closing quote found for string "{0}""#)]
+    UnterminatedString(String),
+    #[error(r#"invalid escape character `{0}` encountered in string "{1}""#)]
+    InvalidEscape(char, String),
 }
